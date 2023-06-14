79ffbe6c855dac8ed44b6d62edaadeaee6200715
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2023-06-14 11:32:13 +0700

    update catalog data seeding

--- src/backend/Optimizely.Server/Controllers/CommerceController.cs
+++ src/backend/Optimizely.Server/Controllers/CommerceController.cs
@@ -12,6 +12,21 @@ using Mediachase.Commerce.Orders.Managers;
 using EPiServer.Commerce.UI.Admin.Warehouses.Internal;
 using EPiServer.Commerce.UI.Admin.Markets.Internal;
 using EPiServer.Shell.Web.Mvc;
+using EPiServer;
+using Optimizely.Server.ExtendedContentTypes;
+using EPiServer.DataAccess;
+using Optimizely.Server.Models;
+using EPiServer.Security;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.SpecializedProperties;
+using Mediachase.Commerce.Catalog;
+using EPiServer.Core;
+using System;
+using Mediachase.Commerce.Pricing;
+using Mediachase.Commerce;
+using Mediachase.Commerce.InventoryService;
+using EPiServer.Commerce.Catalog.Linking;
+using Mediachase.Commerce.Catalog.Dto;
 
 namespace Optimizely.Server.Controllers
 {
@@ -19,14 +34,135 @@ namespace Optimizely.Server.Controllers
     [Authorize(Policy = "ContentManagementAuthorizationPolicy")]
     public class CommerceController : BaseController
     {
+        private readonly IContentRepository _contentRepository;
+        private readonly IRelationRepository _relationRepository;
+        private readonly ReferenceConverter _referenceConverter;
         private readonly IWarehouseRepository _warehouseRepository;
         private readonly IMarketService _marketService;
+        private readonly IPriceDetailService _priceDetailService;
+        private readonly IInventoryService _inventoryService;
 
-        public CommerceController(IWarehouseRepository warehouseRepository,
-            IMarketService marketService)
+        public CommerceController(IContentRepository contentRepository,
+            IRelationRepository relationRepository,
+            ReferenceConverter referenceConverter,
+            IWarehouseRepository warehouseRepository,
+            IMarketService marketService,
+            IPriceDetailService priceDetailService,
+            IInventoryService inventoryService)
         {
+            _contentRepository = contentRepository;
+            _relationRepository = relationRepository;
+            _referenceConverter = referenceConverter;
             _warehouseRepository = warehouseRepository;
             _marketService = marketService;
+            _priceDetailService = priceDetailService;
+            _inventoryService = inventoryService;
+        }
+
+        [HttpPost]
+        [Route("catalogs")]
+        public IActionResult CreateCatalog([FromBody] CatalogModel model)
+        {
+            var rootLink = _referenceConverter.GetRootLink();
+            var catalogs = _contentRepository.GetChildren<CatalogContent>(rootLink);
+
+            if (catalogs.Any(c => c.ContentGuid.ToString().Equals(model.Guid)))
+            {
+                return NoContent();
+            }
+
+            var content = _contentRepository.GetDefault<CatalogContent>(rootLink);
+            content.ContentGuid = new Guid(model.Guid);
+            content.Name = model.CatalogName;
+            content.CatalogLanguages = new ItemCollection<string>(model.CatalogLanguages);
+            content.DefaultLanguage = model.DefaultLanguage;
+            content.DefaultCurrency = model.DefaultCurrency;
+            content.WeightBase = model.WeightBase;
+            content.Owner = model.Owner;
+            content.LengthBase = model.LengthBase;
+            _contentRepository.Save(content, SaveAction.Publish, AccessLevel.NoAccess);
+
+            return Ok($"Catalog \"{content.Name}\" created.");
+        }
+
+        [HttpPost]
+        [Route("categories")]
+        public IActionResult CreateCategory([FromBody] NodeModel model)
+        {
+            var parent = _contentRepository.Get<IContent>(new Guid(model.ParentGuid));
+            var nodes = _contentRepository.GetChildren<GenericNode>(parent.ContentLink);
+
+            if (nodes.Any(c => c.ContentGuid.ToString().Equals(model.Guid)))
+            {
+                return NoContent();
+            }
+
+            var content = _contentRepository.GetDefault<GenericNode>(parent.ContentLink);
+            content.ContentGuid = new Guid(model.Guid);
+            content.Code = model.Code;
+            content.Name = model.Name;
+            _contentRepository.Save(content, SaveAction.Publish, AccessLevel.NoAccess);
+
+            return Ok($"Category \"{content.Name}\" created.");
+        }
+
+        [HttpPost]
+        [Route("products")]
+        public IActionResult CreateProduct([FromBody] ProductModel model)
+        {
+            var parent = _contentRepository.Get<IContent>(new Guid(model.ParentGuid));
+            var products = _contentRepository.GetChildren<GenericProduct>(parent.ContentLink);
+
+            if (products.Any(c => c.ContentGuid.ToString().Equals(model.Guid)))
+            {
+                return NoContent();
+            }
+
+            var content = _contentRepository.GetDefault<GenericProduct>(parent.ContentLink);
+            content.ContentGuid = new Guid(model.Guid);
+            content.Code = model.Code;
+            content.Name = model.Name;
+
+            _contentRepository.Save(content, SaveAction.Publish, AccessLevel.NoAccess);
+
+            return Ok($"Product \"{content.Name}\" created.");
+        }
+
+        [HttpPost]
+        [Route("variants")]
+        public IActionResult CreateVariant([FromBody] VariantModel model)
+        {
+            var parent = _contentRepository.Get<IContent>(new Guid(model.ParentGuid));
+            var variants = _relationRepository.GetChildren<ProductVariation>(parent.ContentLink);
+
+            foreach (var item in variants)
+            {
+                if (_contentRepository.Get<IContent>(item.Child).ContentGuid.ToString().Equals(model.Guid))
+                {
+                    return NoContent();
+                }
+            }
+
+            var content = _contentRepository.GetDefault<GenericVariant>(parent.ContentLink);
+            content.ContentGuid = new Guid(model.Guid);
+            content.Code = model.Code;
+            content.Name = model.Name;
+            (content as IDimensionalStockPlacement).MinQuantity = 1;
+            (content as IDimensionalStockPlacement).MaxQuantity = 100;
+            (content as IDimensionalStockPlacement).Weight = 5;
+            (content as IDimensionalStockPlacement).TrackInventory = true;
+            _contentRepository.Save(content, SaveAction.Publish, AccessLevel.NoAccess);
+
+            foreach (var item in model.VariantPricing)
+            {
+                AddPrice(model.Code, item.MarketId, item.Price, item.CurrencyCode);
+            }
+            foreach (var item in model.VariantInventory)
+            {
+                AddInventory(model.Code, item.InStockQuantity, item.WarehouseCode);
+            }
+
+            return Ok($"Variant \"{content.Name}\" created.");
         }
 
         [HttpPost]
@@ -37,7 +173,7 @@ namespace Optimizely.Server.Controllers
             {
                 _marketService.CreateMarket(new MarketEditModel(model));
 
-                return Ok($"Market {model.MarketName} created.");
+                return Ok($"Market \"{model.MarketName}\" created.");
             }
             else
             {
@@ -82,7 +218,7 @@ namespace Optimizely.Server.Controllers
                 };
 
                 _warehouseRepository.Save(warehouse);
-                return Ok($"Warehouse {warehouse.Name} created.");
+                return Ok($"Warehouse \"{warehouse.Name}\" created.");
             }
             else
             {
@@ -146,5 +282,43 @@ namespace Optimizely.Server.Controllers
                 return this.JsonData(Enumerable.Empty<string>());
             }
         }
+
+        #region Util Functions
+        private void AddPrice(string entryCode, string marketId, decimal price, string currencyCode)
+        {
+            var priceValue = new PriceDetailValue
+            {
+                CatalogKey = new CatalogKey(entryCode),
+                MarketId = marketId,
+                CustomerPricing = CustomerPricing.AllCustomers,
+                MinQuantity = 0,
+                ValidFrom = DateTime.Now.AddDays(-7),
+                ValidUntil = DateTime.Now.AddYears(10),
+                UnitPrice = new Money(price, currencyCode)
+            };
+
+            _priceDetailService.Save(priceValue);
+        }
+
+        private void AddInventory(string entryCode, decimal inStockQuantity, string warehouseCode)
+        {
+            if (inStockQuantity <= 0) return;
+
+            var inventoryRecord = new InventoryRecord()
+            {
+                CatalogEntryCode = entryCode,
+                PurchaseAvailableQuantity = inStockQuantity,
+                BackorderAvailableQuantity = 0,
+                BackorderAvailableUtc = DateTime.UtcNow,
+                IsTracked = true,
+                PreorderAvailableQuantity = 0,
+                PreorderAvailableUtc = DateTime.UtcNow,
+                PurchaseAvailableUtc = DateTime.UtcNow,
+                WarehouseCode = warehouseCode
+            };
+
+            _inventoryService.Save(new[] { inventoryRecord });
+        }
+        #endregion
     }
 }
---
+++ src/backend/Optimizely.Server/Models/VariantModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Optimizely.Server.Models
+{
+    public class VariantModel
+    {
+        public string Guid { get; set; }
+        public string ParentGuid { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public List<VariantPricingModel> VariantPricing { get; set; }
+        public List<VariantInventoryModel> VariantInventory { get; set; }
+    }
+
+    public class VariantPricingModel
+    {
+        public string MarketId { get; set; }
+        public decimal Price { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+
+    public class VariantInventoryModel
+    {
+        public string WarehouseCode { get; set; }
+        public int InStockQuantity { get; set; }
+    }
+}
